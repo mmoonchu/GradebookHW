d80bddde7db1697679b01d94d1adcb68cd5b5450
Moon Chu <mmoonchu2@gmail.com>
2023-11-22 23:07:04 -0500

    Created working 'gradebook'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
 
-function App() {
+function App(props) {
+  const studentName = useRef(null);
+  const grade = useRef(null);
+
+  // State to hold the form data
+  const [forms, setForms] = useState([]);
+
+  const handleChange = () => {
+    setForms([...forms, {'name': studentName.current.value, 'grade': grade.current.value}]);
+    studentName.current.value = '';
+    grade.current.value = '';
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    handleChange();
+    console.log(`kjn`);
+  }
+
+  const displayList = () => {
+    return forms.map((row, index) => (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.grade}</td>
+      </tr>
+    ));
+  }
+
+  // const [tableData, setTableData] = useState(forms);
+
+  // useEffect(() => {
+  //   setTableData(displayList());
+  // }, [forms])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          // value={forms.name}
+          // name="name"
+          ref={studentName}
+          placeholder="Name"
+        />
+        <input
+          type="number"
+          // value={forms.grade}
+          // name="grade"
+          ref={grade}
+          placeholder="Grade"
+        />
+        <input type="submit" value="Submit Form" />
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Student</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayList()}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
